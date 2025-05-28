5f8a81749b6a3af9d972729d246dfe2048eeae62
aperanto-lefko <polarster@yandex.ru>
2025-05-28 10:51:29 +0300

    Добавление application, реализация gateway, config и discovery

--- core/main-service/src/main/java/ru/practicum/MainApplication.java
+++ core/main-service/src/main/java/ru/practicum/MainApplication.java
@@ -1,5 +1,6 @@
 package ru.practicum;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,5 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+    @PostConstruct
+    public void logPort() {
+        System.out.println("Actual port: " + System.getProperty("server.port"));
     }
 }
