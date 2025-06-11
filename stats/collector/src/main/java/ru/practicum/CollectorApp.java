0ec65059f0fce39c049c733c21e5420c8dfb1d61
aperanto-lefko <polarster@yandex.ru>
2025-06-11 12:49:22 +0300

    Создание файлов proto, avro и маппера (добавление файлов)

---
+++ stats/collector/src/main/java/ru/practicum/CollectorApp.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ru.yandex.practicum")
+public class CollectorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorApp.class, args);
+    }
+}
